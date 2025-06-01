1faaf1ed422b696f03e41940684ccbe7e4090a57
Daksh Mishra <147617255+idaxon@users.noreply.github.com>
2025-06-02 01:29:28 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'PitchX',
   description: 'Created with Next.js',
-  generator: 'Dax',
+  generator: 'v0.dev',
 }
 
 export default function RootLayout({

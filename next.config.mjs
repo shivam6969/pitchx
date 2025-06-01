409effa9ee50d2c8489656c389a163d48ecb0434
Daksh Mishra <147617255+idaxon@users.noreply.github.com>
2025-06-02 02:41:18 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/team-3',
+        destination: '/about-us',
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 }
 
 export default nextConfig
